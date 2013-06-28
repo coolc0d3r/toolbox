f31562ff792aa44f8382b3c4e35a99af64e84e0a
Ian Mac <webguyian@gmail.com>
2013-06-27 21:05:55 -0400

    Set homepage route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Toolbox::Application.routes.draw do
   resources :resources
 
+  root :to => "resources#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
