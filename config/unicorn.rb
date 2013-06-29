7fe2ccd583b23c3e3bd8726c82f90dda05fee411
Ian Mac <webguyian@gmail.com>
2013-06-29 11:28:36 -0400

    Upate Unicorn config files

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,47 @@
 root = "/home/deployer/apps/toolbox/current"
+app_directory = "/home/deployer/apps/toolbox/current"
 working_directory root
+
+listen "#{root}/tmp/unicorn.toolbox.sock"
+worker_processes 2
+timeout 60
+
+preload_app true
+
 pid "#{root}/tmp/pids/unicorn.pid"
+
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.toolbox.sock"
-worker_processes 2
-timeout 30
+before_fork do |server, worker|
+  ##
+  # When sent a USR2, Unicorn will suffix its pidfile with .oldbin and
+  # immediately start loading up a new version of itself (loaded with a new
+  # version of our app). When this new Unicorn is completely loaded
+  # it will begin spawning workers. The first worker spawned will check to
+  # see if an .oldbin pidfile exists. If so, this means we've just booted up
+  # a new Unicorn and need to tell the old one that it can now die. To do so
+  # we send it a QUIT.
+  #
+  # Using this method we get 0 downtime deploys.
+ 
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+  # the following is recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+end
+ 
+after_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+end
--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -82,3 +82,72 @@ reopen-logs)
   exit 1
   ;;
 esac
+
+setup () {
+    CONFIG=$1
+ 
+    if [ -z $APP_NAME ]; then
+        echo "App name is not defined in ${CONFIG}"
+        return 1
+    fi
+    if [ -z $APP_USER ]; then
+        echo "App name is not defined in ${CONFIG}"
+        return 1
+    fi
+    if [ -z $RAILS_ROOT ]; then
+        echo "Rails root is not defined in ${CONFIG}"
+        return 1
+    fi
+    if [ -z $RAILS_ENV ]; then
+        echo "Rails environment is not defined in ${CONFIG}"
+        return 1
+    fi
+    # If unicorn binary was not defined in config
+    if [ -z $UNICORN ]; then
+      UNICORN="${RAILS_ROOT}/bin/unicorn"
+    fi
+ 
+    echo "Launching ${APP_NAME} (${RAILS_ROOT})"
+ 
+    cd $RAILS_ROOT || return 1
+    export PID=$RAILS_ROOT/tmp/pids/unicorn.pid
+ 
+    # SUDOCMD="rvmsudo -u ${APP_USER}"
+    SUDOCMD="sudo -u ${APP_USER}"
+ 
+    CMD="${SUDOCMD} RAILS_ENV=${RAILS_ENV} ${UNICORN} -E ${RAILS_ENV} -c ${RAILS_ROOT}/config/unicorn.rb -D"
+    return 0
+}
+
+# either run the start/stop/reload/etc command for every config under /etc/unicorn
+# or just do it for a specific one
+ 
+# $1 contains the start/stop/etc command
+# $2 if it exists, should be the specific config we want to act on
+ 
+if [ $2 ]; then
+  . /etc/unicorn/$2.conf
+  setup "/etc/unicorn/$2.conf"
+  if [ $? -eq 1 ]; then
+    exit
+  fi
+  cmd $1
+else
+  for CONFIG in /etc/unicorn/*.conf; do
+    # clean variables from prev configs
+    unset APP_NAME
+    unset APP_USER
+    unset RAILS_ROOT
+    unset RAILS_ENV
+    unset UNICORN
+ 
+    # import the variables
+    . $CONFIG
+    setup $CONFIG
+    if [ $? -eq 1 ]; then
+      continue
+    fi
+    # run the start/stop/etc command
+    cmd $1
+  done
+fi
