f9d0025bd8adea8dba2f15889d8478ee50bdf44f
Ian Mac <webguyian@gmail.com>
2013-06-29 11:34:04 -0400

    Change Unicorn init script

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -11,77 +11,50 @@
 set -e
 
 # Feel free to change any of the following variables for your app:
-TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/deployer/apps/toolbox/current
-PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-AS_USER=deployer
-set -u
+# TIMEOUT=${TIMEOUT-60}
+# APP_ROOT=/home/deployer/apps/toolbox/current
+# PID=$APP_ROOT/tmp/pids/unicorn.pid
+# CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+# AS_USER=deployer
+# set -u
 
-OLD_PIN="$PID.oldbin"
-
+DEFAULT_USER=deployer
+ 
 sig () {
-  test -s "$PID" && kill -$1 `cat $PID`
+    test -s "$PID" && kill -$1 `cat "$PID"`
 }
-
-oldsig () {
-  test -s $OLD_PIN && kill -$1 `cat $OLD_PIN`
-}
-
-run () {
-  if [ "$(id -un)" = "$AS_USER" ]; then
-    eval $1
-  else
-    su -c "$1" - $AS_USER
-  fi
-}
-
-case "$1" in
-start)
-  sig 0 && echo >&2 "Already running" && exit 0
-  run "$CMD"
-  ;;
-stop)
-  sig QUIT && exit 0
-  echo >&2 "Not running"
-  ;;
-force-stop)
-  sig TERM && exit 0
-  echo >&2 "Not running"
-  ;;
-restart|reload)
-  sig HUP && echo reloaded OK && exit 0
-  echo >&2 "Couldn't reload, starting '$CMD' instead"
-  run "$CMD"
-  ;;
-upgrade)
-  if sig USR2 && sleep 2 && sig 0 && oldsig QUIT
-  then
-    n=$TIMEOUT
-    while test -s $OLD_PIN && test $n -ge 0
-    do
-      printf '.' && sleep 1 && n=$(( $n - 1 ))
-    done
-    echo
-
-    if test $n -lt 0 && test -s $OLD_PIN
-    then
-      echo >&2 "$OLD_PIN still exists after $TIMEOUT seconds"
+ 
+cmd () {
+ 
+  case $1 in
+    start)
+      sig 0 && echo >&2 "Already running" && return 0
+      eval $CMD
+      echo "Starting"
+      ;;
+    stop)
+      sig QUIT && echo "Stopping" && return 0
+      echo >&2 "Not running"
+      ;;
+    force-stop)
+      sig TERM && echo "Forcing a stop" && return 0
+      echo >&2 "Not running"
+      ;;
+    restart|reload|upgrade)
+      sig USR2 && echo Upgraded && return 0
+      echo >&2 "Couldn't upgrade, starting '$CMD' instead"
+      eval $CMD
+      ;;
+    rotate)
+      sig USR1 && echo rotated logs OK && return 0
+      echo >&2 "Couldn't rotate logs" && return 1
+      ;;
+    *)
+      echo >&2 "Usage: $0 <start|stop|restart|upgrade|rotate|force-stop>"
       exit 1
-    fi
-    exit 0
-  fi
-  echo >&2 "Couldn't upgrade, starting '$CMD' instead"
-  run "$CMD"
-  ;;
-reopen-logs)
-  sig USR1
-  ;;
-*)
-  echo >&2 "Usage: $0 <start|stop|restart|upgrade|force-stop|reopen-logs>"
-  exit 1
-  ;;
-esac
+      ;;
+    esac
+}
 
 setup () {
     CONFIG=$1
